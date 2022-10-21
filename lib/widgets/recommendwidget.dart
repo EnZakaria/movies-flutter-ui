5983669eab433c90e1e98ccb92aaee06b5601fe3
zakaria anouar <contact@zakariaanouar.com>
2022-10-21 19:37:25 +0100

    first commit

---
+++ lib/widgets/recommendwidget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecommendWidget extends StatelessWidget {
+  const RecommendWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
